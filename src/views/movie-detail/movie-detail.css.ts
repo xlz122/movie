da58c5c18a618fdbd2bafbc770c22421aae075d6
xlz122 <122874722@qq.com>
2022-09-22 17:10:46 +0800

    feat: 完成影视详情-慕影评分

---
+++ src/views/movie-detail/movie-detail.css.ts
@@ -0,0 +1,62 @@
+import { StyleSheet } from 'react-native';
+import { viewHeight } from '../../utils/screen';
+
+const styles = StyleSheet.create({
+  page: {
+    height: viewHeight - 42 - 44,
+    backgroundColor: '#f5f5f5'
+  },
+  comment: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 44,
+    backgroundColor: '#fff',
+    borderTopWidth: 0.3,
+    borderStyle: 'solid',
+    borderColor: '#eee'
+  },
+  review: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 14,
+    paddingRight: 14,
+    marginLeft: 5
+  },
+  reviewIcon: {
+    marginRight: 6,
+    fontFamily: 'iconfont',
+    fontSize: 16,
+    color: '#7f889b'
+  },
+  reviewText: {
+    fontSize: 12,
+    color: '#7f889b'
+  },
+  tool: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 12
+  },
+  toolItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: 41
+  },
+  toolItemIcon: {
+    fontFamily: 'iconfont',
+    fontSize: 16,
+    color: '#7f889b'
+  },
+  toolItemText: {
+    fontSize: 10,
+    color: '#7f889b'
+  }
+});
+
+export default styles;
