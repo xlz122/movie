668bf022c6e457ae3d329c6f030e019f1e31ec45
xlz122 <122874722@qq.com>
2022-09-27 15:47:00 +0800

    feat: 完成关注影人页面

---
+++ src/views/actor/Actor.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { viewHeight } from '@/utils/screen';
+import { userActors } from '@/api/mine';
+import type { ResponseType, Navigation } from '@/types/index';
+import ScrollRefresh from '@/components/scroll-refresh/ScrollRefresh';
+
+function Actor(): React.ReactElement {
+  const navigation: Navigation = useNavigation();
+
+  const getUserActors = ({ page, per_page }): Promise<unknown[]> => {
+    return new Promise((resolve, reject) => {
+      userActors({ page, per_page })
+        .then((res: ResponseType<unknown[]>) => {
+          if (res.code === 200) {
+            resolve(res.data!);
+          } else {
+            reject();
+          }
+        })
+        .catch(() => ({}));
+    });
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      activeOpacity={1}
+      onPress={() => navigation.push('ActorDetail', { id: item.id })}
+    >
+      <View style={styles.item}>
+        <Image
+          source={{ uri: item.avatar }}
+          resizeMode={'stretch'}
+          style={[styles.itemImage]}
+        />
+        <View style={styles.itemInfo}>
+          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.itemTitle}>
+            {item.name}
+          </Text>
+          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.itemText}>
+            {item.name_en}
+          </Text>
+          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.itemText}>
+            {item.gender}
+            {Boolean(item?.country) && (
+              <>
+                <Text> · </Text>
+                {item?.country}
+              </>
+            )}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.page}>
+      <ScrollRefresh
+        page={1}
+        pageSize={10}
+        request={getUserActors}
+        initialNumToRender={6}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    paddingBottom: Platform.OS !== 'web' ? 10 : 0,
+    width: '100%',
+    height: Platform.OS === 'web' ? viewHeight - 85 : viewHeight + 42 - 85,
+    backgroundColor: '#fff'
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: 16,
+    marginRight: -20,
+    marginLeft: 16
+  },
+  itemImage: {
+    width: 70,
+    height: 92,
+    borderRadius: 3
+  },
+  itemCoverText: {
+    position: 'absolute',
+    top: 1.6,
+    left: 5,
+    zIndex: 1,
+    fontSize: 10,
+    color: '#fff'
+  },
+  itemInfo: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: 13
+  },
+  itemTitle: {
+    marginBottom: 1,
+    fontSize: 13,
+    color: '#333'
+  },
+  itemText: {
+    marginTop: 8,
+    fontSize: 11,
+    color: '#999'
+  }
+});
+
+export default Actor;
