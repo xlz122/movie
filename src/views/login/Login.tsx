256119f5a8b4783591da7e021b42d04bdf7d2f9e
xlz122 <122874722@qq.com>
2022-09-30 15:56:15 +0800

    feat: 调整跳转登录以及关闭登录页的按钮生效范围

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -28,11 +28,9 @@ function Login(): React.ReactElement {
 
   return (
     <View style={styles.login}>
-      <View style={styles.close}>
-        <Text onPress={close} style={styles.closeIcon}>
-          {'\ue612'}
-        </Text>
-      </View>
+      <TouchableOpacity activeOpacity={1} onPress={close} style={styles.close}>
+        <Text style={styles.closeIcon}>{'\ue612'}</Text>
+      </TouchableOpacity>
       {state.type === 'login' && <LoginForm />}
       {state.type === 'register' && <RegisterForm />}
       <View style={styles.tool}>
@@ -58,6 +56,7 @@ const styles = StyleSheet.create({
   close: {
     paddingLeft: 16,
     paddingTop: 16,
+    width: 52,
     height: 52
   },
   closeIcon: {
