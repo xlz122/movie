35ea408c6993d39bb0c17e95b536399884ddc4a5
xlz122 <122874722@qq.com>
2022-11-28 11:46:00 +0800

    fix: 修复影人详情、角色详情移动端滚动问题

--- src/views/role-detail/RoleDetail.tsx
+++ src/views/role-detail/RoleDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useLayoutEffect, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { roleDetail } from '@/api/role';
 import type { RouteProp } from '@react-navigation/native';
@@ -13,6 +13,7 @@ import RoleInfo from './role-info/RoleInfo';
 import RolePhoto from './role-photo/RolePhoto';
 import RoleMovie from './role-movie/RoleMovie';
 import RoleActor from './role-actor/RoleActor';
+import styles from './role-detail.css';
 
 type Route = RouteProp<{ params: { id: number } }>;
 
@@ -73,121 +74,64 @@ function ActorDetail(): React.ReactElement {
   }, []);
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} style={styles.page}>
-      <RoleInfo detail={detail} refreshDetail={refreshDetail} />
-      <View style={styles.count}>
-        <View style={styles.countItem}>
-          <Text style={styles.countItemValue}>
-            {detail?.collection_count}人
-          </Text>
-          <Text style={styles.countItemLabel}>关注数</Text>
-        </View>
-        <View style={styles.countItem}>
-          <Text style={styles.countItemValue}>{detail?.movie_count}部</Text>
-          <Text style={styles.countItemLabel}>影视数</Text>
-        </View>
-        <View style={[styles.countItem, styles.countLastItem]}>
-          <Text style={styles.countItemValue}>{detail?.actor_count}个</Text>
-          <Text style={styles.countItemLabel}>影人数</Text>
-        </View>
-      </View>
-      <Panel title="个人简介" subtitle={'更多信息'}>
-        {Boolean(detail?.brief) && (
-          <Text numberOfLines={4} ellipsizeMode="tail" style={styles.summary}>
-            {detail?.brief}
-          </Text>
-        )}
-        {!detail?.brief && (
-          <View style={styles.noSummary}>
-            <Text style={styles.noSummaryText}>暂无简介</Text>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.page}>
+        <RoleInfo detail={detail} refreshDetail={refreshDetail} />
+        <View style={styles.count}>
+          <View style={styles.countItem}>
+            <Text style={styles.countItemValue}>
+              {detail?.collection_count}人
+            </Text>
+            <Text style={styles.countItemLabel}>关注数</Text>
           </View>
+          <View style={styles.countItem}>
+            <Text style={styles.countItemValue}>{detail?.movie_count}部</Text>
+            <Text style={styles.countItemLabel}>影视数</Text>
+          </View>
+          <View style={[styles.countItem, styles.countLastItem]}>
+            <Text style={styles.countItemValue}>{detail?.actor_count}个</Text>
+            <Text style={styles.countItemLabel}>影人数</Text>
+          </View>
+        </View>
+        <Panel title="个人简介" subtitle={'更多信息'}>
+          {Boolean(detail?.brief) && (
+            <Text numberOfLines={4} ellipsizeMode="tail" style={styles.summary}>
+              {detail?.brief}
+            </Text>
+          )}
+          {!detail?.brief && (
+            <View style={styles.noSummary}>
+              <Text style={styles.noSummaryText}>暂无简介</Text>
+            </View>
+          )}
+        </Panel>
+        {detail?.photos && detail?.photos?.length > 0 && (
+          <Panel title="相册" subtitle={`全部${detail?.photos?.length}张`}>
+            <RolePhoto photo={detail?.photos} />
+          </Panel>
         )}
-      </Panel>
-      {detail?.photos && detail?.photos?.length > 0 && (
-        <Panel title="相册" subtitle={`全部${detail?.photos?.length}张`}>
-          <RolePhoto photo={detail?.photos} />
-        </Panel>
-      )}
-      {detail?.movies && detail?.movies?.length > 0 && (
-        <Panel
-          title="角色影视"
-          subtitle={`全部${detail?.movie_count}部`}
-          panelStyle={{ paddingBottom: 10 }}
-        >
-          <RoleMovie movie={detail?.movies} />
-        </Panel>
-      )}
-      {detail?.actors && detail?.actors?.length > 0 && (
-        <Panel
-          title="角色影人"
-          subtitle={`全部${detail?.movie_count}部`}
-          panelStyle={{ paddingBottom: 10 }}
-        >
-          <RoleActor movie={detail?.actors} />
-        </Panel>
-      )}
-    </ScrollView>
+        {detail?.movies && detail?.movies?.length > 0 && (
+          <Panel
+            title="角色影视"
+            subtitle={`全部${detail?.movie_count}部`}
+            panelStyle={{ paddingBottom: 10 }}
+          >
+            <RoleMovie movie={detail?.movies} />
+          </Panel>
+        )}
+        {detail?.actors && detail?.actors?.length > 0 && (
+          <Panel
+            title="角色影人"
+            subtitle={`全部${detail?.movie_count}部`}
+            panelStyle={{ paddingBottom: 10 }}
+          >
+            <RoleActor movie={detail?.actors} />
+          </Panel>
+        )}
+      </ScrollView>
+      <View style={styles.placeholder} />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    backgroundColor: '#f5f5f5'
-  },
-  count: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 72,
-    paddingLeft: 10,
-    paddingRight: 10,
-    margin: 10,
-    backgroundColor: '#fff',
-    borderRadius: 4
-  },
-  countItem: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    borderRightWidth: 0.5,
-    borderStyle: 'solid',
-    borderColor: '#e5e5e5'
-  },
-  countItemValue: {
-    fontWeight: '700',
-    fontSize: 14,
-    color: '#303133'
-  },
-  countItemLabel: {
-    fontSize: 12,
-    color: '#888'
-  },
-  countLastItem: {
-    borderRightWidth: 0
-  },
-  summary: {
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    color: '#303133'
-  },
-  noSummary: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-    color: '#303133'
-  },
-  noSummaryText: {
-    height: 75,
-    lineHeight: 75,
-    fontSize: 12,
-    color: '#999'
-  }
-});
 
 export default ActorDetail;
