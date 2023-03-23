72a3d9a7bd4f46330b1e093955c9d320da90e372
xlz122 <122874722@qq.com>
2023-03-23 13:58:53 +0800

    feat: 演员列表由滚动列表改为吸顶列表

--- src/views/actor-list/ActorList.tsx
+++ src/views/actor-list/ActorList.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   Image,
   StyleSheet,
-  ScrollView,
+  FlatList,
   TouchableOpacity,
   Platform
 } from 'react-native';
@@ -12,40 +12,67 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { viewHeight } from '@/utils/screen';
 import { movieActor } from '@/api/movies';
 import type { RouteProp } from '@react-navigation/native';
-import type { ResponseType, Navigation } from '@/types/index';
+import type { Navigation, ResponseType } from '@/types/index';
 
 type Route = RouteProp<{ params: { movieId: number } }>;
 
-type ActorType = {
-  id: number;
-  name: string;
-  children: unknown[];
+type ComingType = {
+  list: ItemType[];
+  stickyIndex: number[];
 };
 
 type ItemType = {
-  item: {
-    id: number;
-    avatar: string;
-    name: string;
-    name_en: string;
-    gender: string;
-    country: string;
-  };
-  index: number;
-  length: number;
+  stickyTitle?: string;
+  id?: number;
+  avatar?: string;
+  name?: string;
+  name_en?: string;
+  gender?: string;
+  country?: string;
+  count?: number;
+  lastItem?: boolean;
 };
 
 function ActorList(): React.ReactElement {
   const navigation: Navigation = useNavigation();
   const route: Route = useRoute();
 
-  const [actor, setActor] = useState<ActorType[]>([]);
+  const [actor, setActor] = useState<ComingType>({
+    list: [],
+    stickyIndex: []
+  });
 
-  const getMovieActor = () => {
+  const getMovieActor = (): void => {
     movieActor({ id: route.params.movieId })
-      .then((res: ResponseType<ActorType[]>) => {
+      .then((res: ResponseType<any[]>) => {
         if (res.code === 200) {
-          setActor(res.data!);
+          const list: ItemType[] = [];
+          const stickyIndex: number[] = [];
+
+          res.data?.forEach(item => {
+            const isExist = list.find(t => t.name === item.name);
+
+            // 吸顶标题、索引
+            if (!isExist) {
+              list.push({
+                stickyTitle: item.name,
+                count: item.children.length
+              });
+              stickyIndex.push(list.length - 1);
+            }
+
+            // 演员项
+            item.children.forEach((i: ItemType, ind: number) => {
+              if (ind === item.children.length - 1) {
+                list.push({ ...i, lastItem: true });
+                return false;
+              }
+
+              list.push(i);
+            });
+          });
+
+          setActor({ list, stickyIndex });
         }
       })
       .catch(() => ({}));
@@ -55,123 +82,93 @@ function ActorList(): React.ReactElement {
     getMovieActor();
   }, []);
 
-  const RenderItem = ({ item, index, length }: ItemType) => (
-    <TouchableOpacity
-      activeOpacity={1}
-      onPress={() => navigation.push('ActorDetail', { id: item.id })}
-    >
-      <View
-        style={[
-          styles.listItem,
-          index === length - 1 ? styles.listLastItem : styles.listItem
-        ]}
-      >
-        <Image
-          source={{ uri: item.avatar }}
-          resizeMode={'stretch'}
-          style={[styles.listItemImage]}
-        />
-        <View style={styles.listItemInfo}>
-          <Text style={styles.listItemTitle}>{item.name}</Text>
-          <Text style={styles.listItemText}>{item.name_en}</Text>
-          <Text style={styles.listItemText}>
-            {item?.gender}
-            {Boolean(item?.country) && (
-              <>
-                <Text> · </Text>
-                {item?.country}
-              </>
-            )}
-          </Text>
+  const renderItem = ({ item }: { item: ItemType }) => (
+    <>
+      {item.stickyTitle && (
+        <View style={styles.sticky}>
+          <View style={styles.stickySpot} />
+          <Text style={styles.stickyText}>{item.stickyTitle}</Text>
+          <Text style={styles.stickyCount}>({item.count})</Text>
         </View>
-      </View>
-    </TouchableOpacity>
+      )}
+      {!item.stickyTitle && (
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => navigation.push('ActorDetail', { id: item.id })}
+        >
+          <View
+            style={[styles.item, item.lastItem ? styles.lastItem : styles.item]}
+          >
+            <Image
+              source={{ uri: item.avatar }}
+              resizeMode={'stretch'}
+              style={[styles.itemImage]}
+            />
+            <View style={styles.itemInfo}>
+              <Text style={styles.itemTitle}>{item.name}</Text>
+              <Text style={styles.itemText}>{item.name_en}</Text>
+              <Text style={styles.itemText}>
+                {item?.gender}
+                {Boolean(item?.country) && (
+                  <>
+                    <Text> · </Text>
+                    {item?.country}
+                  </>
+                )}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      )}
+    </>
   );
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} style={styles.page}>
-      <View style={styles.list}>
-        {actor.map((item, index) => {
-          return (
-            <View key={index} style={styles.item}>
-              <View style={styles.itemHeader}>
-                <View style={styles.itemHeaderSpot} />
-                <Text style={styles.itemHeaderText}>{item.name}</Text>
-                <Text style={styles.itemHeaderCount}>
-                  ({item.children.length})
-                </Text>
-              </View>
-              <View style={styles.itemList}>
-                {item?.children?.map((citem, cindex) => {
-                  return (
-                    <RenderItem
-                      key={cindex}
-                      item={citem as ItemType['item']}
-                      index={cindex}
-                      length={item?.children?.length}
-                    />
-                  );
-                })}
-              </View>
-            </View>
-          );
-        })}
-      </View>
-    </ScrollView>
+    <View style={styles.page}>
+      {Boolean(actor.list.length) && (
+        <FlatList
+          stickyHeaderIndices={actor.stickyIndex}
+          data={actor.list}
+          renderItem={renderItem}
+        />
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   page: {
+    paddingBottom: 10,
     // web端需要减去标题高度
     height: Platform.OS === 'web' ? viewHeight - 42 : viewHeight,
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#fff'
   },
-  list: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    flexWrap: 'wrap',
-    alignItems: 'center'
-  },
-  item: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    width: '100%'
-  },
-  itemHeader: {
+  sticky: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     width: '100%',
     height: 45,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
+    backgroundColor: '#f5f5f5'
   },
-  itemHeaderSpot: {
+  stickySpot: {
     width: 4,
     height: 4,
     marginRight: 5,
     backgroundColor: '#f8a52d'
   },
-  itemHeaderText: {
+  stickyText: {
     fontWeight: '700',
     fontSize: 12,
     color: '#303133'
   },
-  itemHeaderCount: {
+  stickyCount: {
     marginLeft: 3,
     fontSize: 10,
     color: '#303133'
   },
-  itemList: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    backgroundColor: '#fff'
-  },
-  listItem: {
+  item: {
     display: 'flex',
     flexDirection: 'row',
     paddingVertical: 8,
@@ -180,28 +177,28 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#dedede'
   },
-  listItemImage: {
+  itemImage: {
     width: 68,
     height: 90,
     borderRadius: 3
   },
-  listItemInfo: {
+  itemInfo: {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
     marginLeft: 13
   },
-  listItemTitle: {
+  itemTitle: {
     marginBottom: 1,
     fontSize: 14,
     color: '#333'
   },
-  listItemText: {
+  itemText: {
     marginTop: 4,
     fontSize: 12,
     color: '#999'
   },
-  listLastItem: {
+  lastItem: {
     borderBottomWidth: 0
   }
 });
