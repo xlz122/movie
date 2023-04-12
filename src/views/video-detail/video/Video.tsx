2c0fc269323338c270d33d9e008cec5ee95b77f9
xlz122 <122874722@qq.com>
2023-04-12 15:21:48 +0800

    feat: 视频详情增加播放工具栏

--- src/views/video-detail/video/Video.tsx
+++ src/views/video-detail/video/Video.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { timeStampToDuration } from '@/utils/utils';
 import { deviceWidth } from '@/utils/screen';
 
 type Props = {
@@ -8,10 +9,19 @@ type Props = {
 
 type InfoType = {
   poster?: string;
+  duration: number;
 };
 
 function Video(props: Props): React.ReactElement {
   const { detail } = props;
+
+  const [play, setPlay] = useState({
+    status: false
+  });
+
+  const playStatusChange = (): void => {
+    setPlay({ status: true });
+  };
 
   return (
     <View style={styles.page}>
@@ -20,9 +30,35 @@ function Video(props: Props): React.ReactElement {
         resizeMode={'stretch'}
         style={[styles.coverImage]}
       />
-      <View style={styles.play}>
-        <Text style={styles.playIcon}>{'\ue616'}</Text>
-      </View>
+      {!play.status && (
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={playStatusChange}
+          style={styles.play}
+        >
+          <Text style={styles.playIcon}>{'\ue616'}</Text>
+        </TouchableOpacity>
+      )}
+      {play.status && (
+        <>
+          <View style={styles.control}>
+            <View style={styles.controlPlay}>
+              <Text style={styles.controlPlayIcon}>{'\ue616'}</Text>
+            </View>
+            <Text style={styles.controlTime}>00:00</Text>
+            <View style={styles.controlProgress} />
+            <Text style={styles.controlTime}>
+              {timeStampToDuration(props.detail.duration)}
+            </Text>
+            <View style={styles.controlClarity}>
+              <Text style={styles.controlClarityText}>高清</Text>
+            </View>
+            <View style={styles.controlFull}>
+              <Text style={styles.controlFullIcon}>{'\ue69a'}</Text>
+            </View>
+          </View>
+        </>
+      )}
     </View>
   );
 }
@@ -55,6 +91,64 @@ const styles = StyleSheet.create({
     fontFamily: 'iconfont',
     fontSize: 16,
     color: '#fff'
+  },
+  control: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: deviceWidth,
+    height: 32,
+    paddingHorizontal: 4
+  },
+  controlPlay: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 30,
+    height: 30
+  },
+  controlPlayIcon: {
+    fontFamily: 'iconfont',
+    fontSize: 18,
+    color: '#fff'
+  },
+  controlTime: {
+    paddingHorizontal: 4,
+    fontSize: 12,
+    color: '#fff'
+  },
+  controlProgress: {
+    flex: 1,
+    height: 3,
+    marginHorizontal: 8,
+    backgroundColor: 'hsla(0, 0%, 100%, 0.3)',
+    borderRadius: 18
+  },
+  controlClarity: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 8
+  },
+  controlClarityText: {
+    fontSize: 12,
+    color: '#e5e5e5'
+  },
+  controlFull: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 30,
+    height: 30,
+    marginRight: 5
+  },
+  controlFullIcon: {
+    fontFamily: 'iconfont',
+    fontSize: 18,
+    color: 'hsla(0, 0%, 100%, 0.85)'
   }
 });
 
